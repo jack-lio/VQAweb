95629f1437737a3b0a3c51d36c3711094c9bdf5e
jack-lio <jack9606796067@gmail.com>
2026-05-14 12:25:10 +0800

    Run FastAPI as Cloud Run main process

--- cloud-run/start.sh
+++ cloud-run/start.sh
@@ -4,28 +4,13 @@ set -eu
 : "${PORT:=8080}"
 : "${BACKEND_PORT:=3000}"
 
-uvicorn main:app --host 127.0.0.1 --port "${BACKEND_PORT}" &
-backend_pid="$!"
-
 envsubst '${PORT} ${BACKEND_PORT}' \
     < /etc/nginx/templates/cloud-run.conf.template \
     > /etc/nginx/nginx.conf
 
-trap 'kill "${backend_pid}" 2>/dev/null || true' INT TERM
-
 nginx -g 'daemon off;' &
 nginx_pid="$!"
 
-while true; do
-    if ! kill -0 "${backend_pid}" 2>/dev/null; then
-        wait "${backend_pid}"
-        exit "$?"
-    fi
+trap 'kill "${nginx_pid}" 2>/dev/null || true' INT TERM
 
-    if ! kill -0 "${nginx_pid}" 2>/dev/null; then
-        wait "${nginx_pid}"
-        exit "$?"
-    fi
-
-    sleep 2
-done
+exec uvicorn main:app --host 127.0.0.1 --port "${BACKEND_PORT}"
